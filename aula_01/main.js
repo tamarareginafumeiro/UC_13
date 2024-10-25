c7d762a8bb3fc6eaf11f78be980b920e4c251f0b
tamarareginafumeiro <tamara.rfumeiro@senacsp.edu.br>
2024-10-24 21:21:00 -0300

    aula_01

---
+++ aula_01/main.js
@@ -0,0 +1,20 @@
+var page = document.getElementById('page');
+    page.innerHTML = 'ok'; 
+
+//var title = document.getElementsByTagName('h1');
+var buttons = document.getElementsByTagName('button');
+
+/*buttons[0].addEventListener('click', function (event) {
+    console.log(event);
+})*/
+
+/*Object.values(title).forEach(function (elemento, index ) {
+        console.log(elemento, index);
+})*/
+
+
+//console.log(title);
+
+function mostrarMensagem() {
+    alert("Parabéns!");
+}
